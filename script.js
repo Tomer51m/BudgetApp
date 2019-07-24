1d6354867849096a1e429c7a0f928b556e200118
Tomer Matmon <tomer.matmon@gmail.com>
2019-07-24 14:40:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Budget controller module
+let budgetController = (function() {
+    //code
+})();
+
+// UI controller module
+let UIController = (function() {
+    //code
+})();
+
+// App controller module
+let appContoller = (function() {
+    
+})();
